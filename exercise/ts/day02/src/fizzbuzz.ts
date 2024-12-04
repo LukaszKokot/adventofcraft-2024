acf01717ee553562b69e8c64f10be73aa7d036a4
Lukasz Kokot <lkokot@kumojin.com>
2024-12-03 22:37:47 -0500

    Use interface

--- exercise/ts/day02/src/fizzbuzz.ts
+++ exercise/ts/day02/src/fizzbuzz.ts
@@ -3,15 +3,13 @@ import { none, Option, some } from "fp-ts/Option";
 export const MIN = 1;
 export const MAX = 100;
 
-export const game = ({
-  mapping,
-  min = MIN,
-  max = MAX,
-}: {
+export interface GameRules {
   mapping: Map<number, string>;
   min?: number;
   max?: number;
-}) => {
+}
+
+export const game = ({ mapping, min = MIN, max = MAX }: GameRules) => {
   const convertSafely = (input: number): string => {
     let result = "";
     for (const [divisor, value] of mapping) {
@@ -24,7 +22,7 @@ export const game = ({
   };
 
   const is = (divisor: number, input: number): boolean => input % divisor === 0;
-  
+
   const isOutOfRange = (input: number): boolean => input < min || input > max;
 
   return {
