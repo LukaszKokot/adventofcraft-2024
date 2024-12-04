885ddf56cda510738692a9bf8807432d9803c68f
Lukasz Kokot <lkokot@kumojin.com>
2024-12-03 22:23:03 -0500

    Format files

--- exercise/ts/day02/src/fizzbuzz.ts
+++ exercise/ts/day02/src/fizzbuzz.ts
@@ -3,29 +3,21 @@ import { none, Option, some } from "fp-ts/Option";
 export const min = 1;
 export const max = 100;
 
-// let mapping: Map<number, string> = new Map([
-//     [15, 'FizzBuzz'],
-//     [3, 'Fizz'],
-//     [5, 'Buzz'],
-// ]);
-
-export const game = (mapping: Map<number, string>) => {
-    const convertSafely = (input: number): string => {
-        for (const [divisor, value] of mapping) {
-            if (is(divisor, input)) {
-                return value;
-            }
-        }
-        return input.toString();
+export const game = ({ mapping }: { mapping: Map<number, string> }) => {
+  const convertSafely = (input: number): string => {
+    for (const [divisor, value] of mapping) {
+      if (is(divisor, input)) {
+        return value;
+      }
     }
+    return input.toString();
+  };
 
-    return {
-        fizzbuzz: (input: number): Option<string> =>
-            isOutOfRange(input)
-                ? none
-                : some(convertSafely(input)),
-    }
-}
+  return {
+    fizzbuzz: (input: number): Option<string> =>
+      isOutOfRange(input) ? none : some(convertSafely(input)),
+  };
+};
 
 const is = (divisor: number, input: number): boolean => input % divisor === 0;
 const isOutOfRange = (input: number): boolean => input < min || input > max;
--- exercise/ts/day02/tests/fizzbuzz.spec.ts
+++ exercise/ts/day02/tests/fizzbuzz.spec.ts
@@ -1,60 +1,68 @@
-import {game, max, min} from '../src/fizzbuzz';
-import * as O from 'fp-ts/Option';
-import {isNone, isSome} from 'fp-ts/Option';
-import * as fc from 'fast-check';
-import {pipe} from 'fp-ts/function';
+import * as fc from "fast-check";
+import { pipe } from "fp-ts/function";
+import * as O from "fp-ts/Option";
+import { isNone, isSome } from "fp-ts/Option";
+import { game, max, min } from "../src/fizzbuzz";
 
-describe('FizzBuzz should return', () => {
-    test.each([
-        [1, '1'],
-        [67, '67'],
-        [82, '82'],
-        [3, 'Fizz'],
-        [66, 'Fizz'],
-        [99, 'Fizz'],
-        [5, 'Buzz'],
-        [50, 'Buzz'],
-        [85, 'Buzz'],
-        [15, 'FizzBuzz'],
-        [30, 'FizzBuzz'],
-        [45, 'FizzBuzz']
-    ])('its representation %s -> %s', (input, expectedResult) => {
-        const conversionResult = fizzbuzzGame.fizzbuzz(input);
-        expect(isSome(conversionResult)).toBeTruthy();
+describe("FizzBuzz should return", () => {
+  const fizzbuzzGame = game({
+    mapping: new Map([
+      [15, "FizzBuzz"],
+      [3, "Fizz"],
+      [5, "Buzz"],
+    ]),
+  });
 
-        if (isSome(conversionResult)) {
-            expect(conversionResult.value).toBe(expectedResult);
-        }
-    });
-
-    test('valid strings for numbers between 1 and 100', () => {
-        fc.assert(
-            fc.property(
-                fc.integer().filter(n => n >= min && n <= max),
-                (n) => isConvertValid(n)
-            )
-        );
-    });
-
-    const fizzbuzzGame = game(new Map([
-        [15, 'FizzBuzz'],
-        [3, 'Fizz'],
-        [5, 'Buzz'],
-    ]));
-
-    const isConvertValid = (input: number): boolean => pipe(
-        fizzbuzzGame.fizzbuzz(input),
-        O.exists(result => validStringsFor(input).includes(result))
+  const isConvertValid = (input: number): boolean =>
+    pipe(
+      fizzbuzzGame.fizzbuzz(input),
+      O.exists((result) => validStringsFor(input).includes(result))
     );
 
-    const validStringsFor = (x: number): string[] => ['Fizz', 'Buzz', 'FizzBuzz', x.toString()];
+  const validStringsFor = (x: number): string[] => [
+    "Fizz",
+    "Buzz",
+    "FizzBuzz",
+    x.toString(),
+  ];
 
-    test('none for numbers out of range', () => {
-        fc.assert(
-            fc.property(
-                fc.integer().filter(n => n < min || n > max),
-                (n) => isNone(fizzbuzzGame.fizzbuzz(n))
-            )
-        );
-    });
+  test.each([
+    [1, "1"],
+    [67, "67"],
+    [82, "82"],
+    [3, "Fizz"],
+    [66, "Fizz"],
+    [99, "Fizz"],
+    [5, "Buzz"],
+    [50, "Buzz"],
+    [85, "Buzz"],
+    [15, "FizzBuzz"],
+    [30, "FizzBuzz"],
+    [45, "FizzBuzz"],
+  ])("its representation %s -> %s", (input, expectedResult) => {
+    const conversionResult = fizzbuzzGame.fizzbuzz(input);
+    expect(isSome(conversionResult)).toBeTruthy();
+
+    if (isSome(conversionResult)) {
+      expect(conversionResult.value).toBe(expectedResult);
+    }
+  });
+
+  test("valid strings for numbers between 1 and 100", () => {
+    fc.assert(
+      fc.property(
+        fc.integer().filter((n) => n >= min && n <= max),
+        (n) => isConvertValid(n)
+      )
+    );
+  });
+
+  test("none for numbers out of range", () => {
+    fc.assert(
+      fc.property(
+        fc.integer().filter((n) => n < min || n > max),
+        (n) => isNone(fizzbuzzGame.fizzbuzz(n))
+      )
+    );
+  });
 });
