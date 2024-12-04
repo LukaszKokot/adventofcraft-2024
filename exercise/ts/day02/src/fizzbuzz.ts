c7d8536bd6856b73fff373eb7649cd08ef91c447
Lukasz Kokot <lkokot@kumojin.com>
2024-12-03 22:48:23 -0500

    ✨ Day 02 : Improve Fizzbuzz game (#3)
    We create a game on top of the existing fizzbuzz function that can
    take a rules configuration, with the mappings, min and max.
    Additionally, the code has been improved in order to automatically
    compute the right combination of strings, instead of having to
    provide them all.

--- exercise/ts/day02/src/fizzbuzz.ts
+++ exercise/ts/day02/src/fizzbuzz.ts
@@ -1,27 +1,33 @@
-import {none, Option, some} from "fp-ts/Option";
+import { none, Option, some } from "fp-ts/Option";
 
-export const min = 1;
-export const max = 100;
+export const MIN = 1;
+export const MAX = 100;
 
-let mapping: Map<number, string> = new Map([
-    [15, 'FizzBuzz'],
-    [3, 'Fizz'],
-    [5, 'Buzz'],
-]);
-
-export const fizzbuzz = (input: number): Option<string> =>
-    isOutOfRange(input)
-        ? none
-        : some(convertSafely(input));
-
-function convertSafely(input: number): string {
-    for (const [divisor, value] of mapping) {
-        if (is(divisor, input)) {
-            return value;
-        }
-    }
-    return input.toString();
+export interface GameRules {
+  mapping: Map<number, string>;
+  min?: number;
+  max?: number;
 }
 
-const is = (divisor: number, input: number): boolean => input % divisor === 0;
-const isOutOfRange = (input: number): boolean => input < min || input > max;
+export const game = ({ mapping, min = MIN, max = MAX }: GameRules) => {
+  const convertSafely = (input: number): string => {
+    let result = "";
+    
+    for (const [divisor, value] of mapping) {
+      if (is(divisor, input)) {
+        result += value;
+      }
+    }
+
+    return result === "" ? input.toString() : result;
+  };
+
+  const is = (divisor: number, input: number): boolean => input % divisor === 0;
+
+  const isOutOfRange = (input: number): boolean => input < min || input > max;
+
+  return {
+    fizzbuzz: (input: number): Option<string> =>
+      isOutOfRange(input) ? none : some(convertSafely(input)),
+  };
+};
