7db0500fbfc7de4cabd9008d885b832b91b7230b
Lukasz Kokot <lkokot@kumojin.com>
2024-12-08 14:05:49 -0500

    ✨ Day 04 : Add testing using test doubles (#5)
    We implement the same test twice: once using jest fn
    implementations, the other using a combination of mocks
    and fakes.

--- exercise/ts/day04/tests/routine.spec.ts
+++ exercise/ts/day04/tests/routine.spec.ts
@@ -1,9 +1,82 @@
-describe('Routine', () => {
-    test('start routine with Jest', () => {
+import { Routine } from "../src/routine/routine";
+import { Schedule } from "../src/routine/schedule";
 
-    });
+describe("Routine", () => {
+  test("start routine with Jest", () => {
+    const fakeTasks = ["task1", "task2", "task3"];
+    const fakeSchedule = { tasks: fakeTasks };
+    const positions = [] as string[];
 
-    test('start routine with manual test doubles', () => {
+    const positionSaver = (position: string) => {
+      positions.push(position);
+    };
+    const fakePositionSaver = (position: string) =>
+      jest.fn().mockImplementation(() => positionSaver(position));
 
-    });
+    const emailService = {
+      readNewEmails: fakePositionSaver("readNewEmails"),
+    };
+    const scheduleService = {
+      todaySchedule: jest.fn().mockReturnValue(fakeSchedule),
+      continueDay: fakePositionSaver("continueDay"),
+      organizeMyDay: fakePositionSaver("organizeMyDay"),
+    };
+    const feeder = {
+      feedReindeers: fakePositionSaver("feedReindeers"),
+    };
+
+    const routine = new Routine(emailService, scheduleService, feeder);
+    routine.start();
+
+    expect(positions).toMatchInlineSnapshot(`
+[
+  "organizeMyDay",
+  "feedReindeers",
+  "readNewEmails",
+  "continueDay",
+]
+`);
+    expect(scheduleService.organizeMyDay).toHaveBeenCalledTimes(1);
+    expect(scheduleService.organizeMyDay.mock.calls[0][0]).toBe(fakeSchedule);
+  });
+
+  test("start routine with manual test doubles", () => {
+    const organizer = { schedule: undefined as Schedule | undefined };
+    const fakeTasks = ["task1", "task2", "task3"];
+    const fakeSchedule = { tasks: fakeTasks };
+    const positions = [] as string[];
+
+    const positionSaver = (position: string) => {
+      positions.push(position);
+    };
+
+    const emailService = {
+      readNewEmails: () => positionSaver("readNewEmails"),
+    };
+    const scheduleService = {
+      organizer,
+      todaySchedule: () => fakeSchedule,
+      continueDay: () => positionSaver("continueDay"),
+      organizeMyDay: (schedule: Schedule) => {
+        organizer.schedule = schedule;
+        positionSaver("organizeMyDay");
+      },
+    };
+    const feeder = {
+      feedReindeers: () => positionSaver("feedReindeers"),
+    };
+
+    const routine = new Routine(emailService, scheduleService, feeder);
+    routine.start();
+
+    expect(positions).toMatchInlineSnapshot(`
+      [
+        "organizeMyDay",
+        "feedReindeers",
+        "readNewEmails",
+        "continueDay",
+      ]
+      `);
+    expect(scheduleService.organizer).toMatchObject({ schedule: fakeSchedule });
+  });
 });
