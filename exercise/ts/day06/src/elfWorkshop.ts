748ad31ba6f32c6e1b4264164125948f5441e666
Lukasz Kokot <lkokot@kumojin.com>
2024-12-09 13:29:52 -0500

    ✨ Day 06 : Add code review comments (#7)
    We add a code review to the existing code in the form of multi-line comments.

--- exercise/ts/day06/src/elfWorkshop.ts
+++ exercise/ts/day06/src/elfWorkshop.ts
@@ -1,16 +1,23 @@
 export class ElfWorkshop {
-    taskList: string[] = [];
+  // We should make that private, and add a method to return the list of tasks.
+  // That method should ensure the list is not mutable by the caller.
+  taskList: string[] = [];
 
-    addTask(task: string): void {
-        if (task !== "") {
-            this.taskList.push(task);
-        }
+  addTask(task: string): void {
+    // Should we check more in depth and ensure the task does not contain only whitespaces?
+    if (task !== "") {
+      this.taskList.push(task);
     }
+  }
 
-    completeTask(): string {
-        if (this.taskList.length > 0) {
-            return this.taskList.shift();
-        }
-        return null;
+  // We could change the signature to indicate the method can return null in case there
+  // are no tasks left.
+  completeTask(): string {
+    // Looks like we could just return the result of the shift operation, and change the
+    // signature to indicate it can return the completed task or undefined.
+    if (this.taskList.length > 0) {
+      return this.taskList.shift();
     }
+    return null;
+  }
 }
