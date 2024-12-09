54b4eccbcc832ab97d786ca39160a4d7ea0085ed
Lukasz Kokot <lkokot@kumojin.com>
2024-12-09 09:47:47 -0500

    Add gender check

--- exercise/ts/day05/src/eid.ts
+++ exercise/ts/day05/src/eid.ts
@@ -1,5 +1,14 @@
-const eid = () => {
-  return "00000000";
+const eid = (gender: string) => {
+  let codifiedGender = "";
+  if (gender === "Sloubi") {
+    codifiedGender = "1";
+  } else if (gender === "Gagna") {
+    codifiedGender = "2";
+  } else if (gender === "Catact") {
+    codifiedGender = "3";
+  }
+
+  return `${codifiedGender}0000000`;
 };
 
 export default eid;
--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -1,13 +1,29 @@
+import fc from "fast-check";
 import eid from "../src/eid";
 
 describe("EID", () => {
-  const result = eid();
+  const defaultEid = eid("Sloubi");
 
   test("is always exactly 8 characters long", () => {
-    expect(result).toHaveLength(8);
+    expect(defaultEid).toHaveLength(8);
   });
 
   test("is composed of numerical characters only", () => {
-    expect(result).toMatch(/^\d+$/);
+    expect(defaultEid).toMatch(/^\d+$/);
+  });
+
+  test("has first character matching gender", () => {
+    fc.assert(
+      fc.property(
+        fc.oneof(
+          fc.constant("Sloubi"),
+          fc.constant("Gagna"),
+          fc.constant("Catact")
+        ),
+        (gender) =>
+          eid(gender)[0] ===
+          (gender === "Sloubi" ? "1" : gender === "Gagna" ? "2" : "3")
+      )
+    );
   });
 });
