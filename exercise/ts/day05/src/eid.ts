03858582d447c7de79343d030249bac3fb418167
Lukasz Kokot <lkokot@kumojin.com>
2024-12-09 09:35:59 -0500

    Add length check

---
+++ exercise/ts/day05/src/eid.ts
@@ -0,0 +1,5 @@
+const eid = () => {
+  return "        ";
+};
+
+export default eid;
--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -1,3 +1,12 @@
+import eid from "../src/eid";
+
+describe("EID", () => {
+  const result = eid();
+
+  test("is always exactly 8 characters long", () => {
+    expect(result).toHaveLength(8);
+  });
+
 describe('EID', () => {
     test('implement your tests here', () => {
         expect(43).toBe(42);
