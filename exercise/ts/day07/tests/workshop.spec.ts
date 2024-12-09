2bdcf0bb0c09ec75817ea5dc7d68ebaef9cdad14
Lukasz Kokot <lkokot@kumojin.com>
2024-12-09 13:53:03 -0500

    ✨ Day 07 : Improve existing tests (#8)
    We improve the existing test by labelling them better, closer to the
    domain itself, and by adding a custom matcher for asserting the
    production status of a gift.

--- exercise/ts/day07/tests/workshop.spec.ts
+++ exercise/ts/day07/tests/workshop.spec.ts
@@ -1,24 +1,56 @@
-import {Gift} from "../src/workshop/gift";
-import {Workshop} from "../src/workshop/workshop";
-import {Status} from "../src/workshop/status";
+import { Gift } from "../src/workshop/gift";
+import { Status } from "../src/workshop/status";
+import { Workshop } from "../src/workshop/workshop";
 
-describe('Workshop', () => {
-    const TOY_NAME = '1 Super Nintendo';
+interface CustomMatchers<R = unknown> {
+  toHaveBeenProduced(): R;
+}
 
-    it('completes a gift and sets its status to produced', () => {
+declare global {
+  namespace jest {
+    interface Expect extends CustomMatchers {}
+    interface Matchers<R> extends CustomMatchers<R> {}
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+describe("Workshop", () => {
+  const TOY_NAME = "1 Super Nintendo";
+  expect.extend({
+    toHaveBeenProduced(gift: Pick<Gift, "getStatus">) {
+      const pass = gift.getStatus() === Status.Produced;
+      return {
+        message: () =>
+          pass
+            ? `expected gift to not have been produced`
+            : `expected gift to have been produced`,
+        pass,
+      };
+    },
+  });
+
+  describe("attempting to complete production of a gift", () => {
+    describe("that was added to production line", () => {
+      it("succeeds and returns the gift with a status indicating successful production", () => {
         const workshop = new Workshop();
         workshop.addGift(new Gift(TOY_NAME));
 
         const completedGift = workshop.completeGift(TOY_NAME);
-
-        expect(completedGift).not.toBeNull();
-        expect(completedGift!.getStatus()).toBe(Status.Produced);
+        if (completedGift === null) {
+          fail("gift should not be null");
+        }
+        expect(completedGift).toBeInstanceOf(Gift);
+        expect(completedGift).toHaveBeenProduced();
+      });
     });
 
-    it('returns null when trying to complete a non-existing gift', () => {
+    describe("that was not added to the production line", () => {
+      it("fails and returns a falsy value", () => {
         const workshop = new Workshop();
-        const completedGift = workshop.completeGift('NonExistingToy');
+        const completedGift = workshop.completeGift(TOY_NAME);
 
         expect(completedGift).toBeNull();
+      });
     });
+  });
 });
