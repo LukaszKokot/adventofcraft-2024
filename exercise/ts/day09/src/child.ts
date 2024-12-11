02b99ee525df2965ef7347d76d45835baf3964e7
Lukasz Kokot <lkokot@kumojin.com>
2024-12-11 10:31:01 -0500

    ✨ Day 09 : Improve tests (#10)
    We use the fast-check library and change the existing tests to
    use record-testing but still have properly named tests, matching
    a domain requirement.

--- exercise/ts/day09/src/child.ts
+++ exercise/ts/day09/src/child.ts
@@ -1,17 +1,19 @@
-import {GiftRequest} from "./giftRequest";
+import { GiftRequest } from "./giftRequest";
 
 export class Child {
-    public readonly name: string;
-    public readonly behavior: Behavior;
-    public readonly giftRequest: GiftRequest;
+  public readonly name: string;
+  public readonly behavior: Behavior;
+  public readonly giftRequest: GiftRequest;
 
-    constructor(firstName: string,
-                lastName: string,
-                age: number,
-                behavior: Behavior,
-                giftRequest: GiftRequest) {
-        this.name = firstName;
-        this.behavior = behavior;
-        this.giftRequest = giftRequest;
-    }
+  constructor(
+    firstName: string,
+    lastName: string,
+    age: number,
+    behavior: Behavior,
+    giftRequest: GiftRequest
+  ) {
+    this.name = firstName;
+    this.behavior = behavior;
+    this.giftRequest = giftRequest;
+  }
 }
