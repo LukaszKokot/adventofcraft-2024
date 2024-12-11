cdf54741ac6daf98fe579fc34dba54fa2b318066
Lukasz Kokot <lkokot@kumojin.com>
2024-12-11 09:24:32 -0500

    Improve the test

--- exercise/ts/day09/tests/santa.analysis.spec.ts
+++ exercise/ts/day09/tests/santa.analysis.spec.ts
@@ -1,40 +1,90 @@
+import fc from "fast-check";
 import { Child } from "../src/child";
 import { GiftRequest } from "../src/giftRequest";
 import { SantaService } from "../src/santaService";
 
-describe("santa analyzing child requests", () => {
+describe("santa analysis", () => {
   const service = new SantaService();
 
-  test("request is approved for nice child with feasible gift", () => {
-    const niceChild = new Child(
-      "Alice",
-      "Thomas",
-      9,
-      "nice",
-      new GiftRequest("Bicycle", true, "nice to have")
+  fc.configureGlobal({ numRuns: 100 });
+
+  const validRecord = ({
+    behavior,
+    isFeasible,
+  }: {
+    behavior?: Behavior;
+    isFeasible?: boolean;
+  }) =>
+    fc.record({
+      name: fc.string(),
+      firstName: fc.string(),
+      behavior: fc.oneof(
+        fc.constant(behavior) ??
+          (fc.constant<Behavior>("nice"), fc.constant<Behavior>("naughty"))
+      ),
+      giftRequest: fc.record({
+        giftName: fc.string(),
+        isFeasible: fc.constant(isFeasible) ?? fc.boolean(),
+        priority: fc.oneof(
+          fc.constant<Priority>("dream"),
+          fc.constant<Priority>("nice to have")
+        ),
+      }),
+    });
+
+  const recordToChild = (record: {
+    name: string;
+    firstName: string;
+    behavior: Behavior;
+    giftRequest: {
+      giftName: string;
+      isFeasible: boolean;
+      priority: Priority;
+    };
+  }): Child =>
+    new Child(
+      record.name,
+      record.firstName,
+      10,
+      record.behavior,
+      new GiftRequest(
+        record.giftRequest.giftName,
+        record.giftRequest.isFeasible,
+        record.giftRequest.priority
+      )
     );
-    expect(service.evaluateRequest(niceChild)).toBeTruthy();
+
+  describe("should deny the gift", () => {
+    it("when it is not feasible to build it", () => {
+      fc.assert(
+        fc.property(validRecord({ isFeasible: false }), (record) => {
+          const child = recordToChild(record);
+          expect(service.evaluateRequest(child)).toBe(false);
+        })
+      );
+    });
+
+    it("when the child has been naughty", () => {
+      fc.assert(
+        fc.property(validRecord({ behavior: "naughty" }), (record) => {
+          const child = recordToChild(record);
+          expect(service.evaluateRequest(child)).toBe(false);
+        })
+      );
+    });
   });
 
-  test("request is denied for naughty child", () => {
-    const naughtyChild = new Child(
-      "Noa",
-      "Thierry",
-      6,
-      "naughty",
-      new GiftRequest("SomeToy", true, "dream")
-    );
-    expect(service.evaluateRequest(naughtyChild)).toBeFalsy();
-  });
-
-  test("request is denied for nice child with infeasible gift", () => {
-    const niceChildWithInfeasibleGift = new Child(
-      "Charlie",
-      "Joie",
-      3,
-      "nice",
-      new GiftRequest("AnotherToy", false, "dream")
-    );
-    expect(service.evaluateRequest(niceChildWithInfeasibleGift)).toBeFalsy();
+  describe("should approve the gift", () => {
+    it("when it is feasible to build it and the child has been nice", () => {
+      fc.assert(
+        fc.property(
+          validRecord({ isFeasible: true, behavior: "nice" }),
+          (record) => {
+            const child = recordToChild(record);
+            expect(service.evaluateRequest(child)).toBe(true);
+          }
+        )
+      );
+    });
   });
 });
