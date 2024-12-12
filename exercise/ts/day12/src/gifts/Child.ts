0c3acfbac009a857027a44fca4890239879764a7
Lukasz Kokot <lkokot@kumojin.com>
2024-12-12 10:02:45 -0500

    Type behaviour

--- exercise/ts/day12/src/gifts/Child.ts
+++ exercise/ts/day12/src/gifts/Child.ts
@@ -1,9 +1,10 @@
+import { Behaviour } from "./Behaviour";
 import { Toy } from "./Toy";
 
 export class Child {
   public wishlist: Toy[] = [];
 
-  constructor(public name: string, public behavior: string) {}
+  constructor(public name: string, public behavior: Behaviour) {}
 
   setWishlist(firstChoice: Toy, secondChoice: Toy, thirdChoice: Toy): void {
     this.wishlist = [firstChoice, secondChoice, thirdChoice];
