7fb8f34a70fd25e8a046214c555fad09d8f4ccff
Lukasz Kokot <lkokot@kumojin.com>
2024-12-12 09:53:07 -0500

    ✨ Day 11 : Improve features and tests (#12)
    We improve the existing code to take care of unexpected input
    values, and also improve the tests themselves to make them
    more resilient.

--- exercise/ts/day11/src/christmas/preparation.ts
+++ exercise/ts/day11/src/christmas/preparation.ts
@@ -1,38 +1,58 @@
-import {ToyType} from "./toyType";
+import { ToyBalance, ToyType } from "./toyType";
+
+export enum GiftPreparation {
+  NO_ONE = "No gifts to prepare.",
+  ELVES = "Elves will prepare the gifts.",
+  SANTA = "Santa will prepare the gifts.",
+}
+
+export enum GiftCategory {
+  BABY = "Baby",
+  TODDLER = "Toddler",
+  CHILD = "Child",
+  TEEN = "Teen",
+}
+
+export const MAX_GIFTS_FOR_ELVES = 50;
+export const MAX_AGE_FOR_BABY = 2;
+export const MAX_AGE_FOR_TODDLER = 5;
+export const MAX_AGE_FOR_CHILD = 12;
 
 export class Preparation {
-    static prepareGifts(numberOfGifts: number): string {
-        if (numberOfGifts <= 0) {
-            return 'No gifts to prepare.';
-        } else if (numberOfGifts < 50) {
-            return 'Elves will prepare the gifts.';
-        }
-        return 'Santa will prepare the gifts.';
+  static prepareGifts(numberOfGifts: number): GiftPreparation {
+    switch (true) {
+      case numberOfGifts <= 0:
+        return GiftPreparation.NO_ONE;
+      case numberOfGifts < MAX_GIFTS_FOR_ELVES:
+        return GiftPreparation.ELVES;
+      default:
+        return GiftPreparation.SANTA;
+    }
+  }
+
+  static categorizeGift(age: number): string {
+    switch (true) {
+      case age <= MAX_AGE_FOR_BABY:
+        return GiftCategory.BABY;
+      case age <= MAX_AGE_FOR_TODDLER:
+        return GiftCategory.TODDLER;
+      case age <= MAX_AGE_FOR_CHILD:
+        return GiftCategory.CHILD;
+      default:
+        return GiftCategory.TEEN;
+    }
+  }
+
+  static ensureToyBalance(
+    toyType: ToyType,
+    toysCount: number,
+    totalToys: number
+  ): boolean {
+    if (!ToyBalance.has(toyType)) {
+      return false;
     }
 
-    static categorizeGift(age: number): string {
-        if (age <= 2) {
-            return 'Baby';
-        } else if (age <= 5) {
-            return 'Toddler';
-        } else if (age <= 12) {
-            return 'Child';
-        }
-        return 'Teen';
-    }
-
-    static ensureToyBalance(toyType: ToyType, toysCount: number, totalToys: number): boolean {
-        const typePercentage = toysCount / totalToys;
-
-        switch (toyType) {
-            case ToyType.EDUCATIONAL:
-                return typePercentage >= 0.25;
-            case ToyType.FUN:
-                return typePercentage >= 0.30;
-            case ToyType.CREATIVE:
-                return typePercentage >= 0.20;
-            default:
-                return false;
-        }
-    }
+    const typePercentage = toysCount / totalToys;
+    return typePercentage >= ToyBalance.get(toyType);
+  }
 }
--- exercise/ts/day11/tests/preparation.spec.ts
+++ exercise/ts/day11/tests/preparation.spec.ts
@@ -1,34 +1,88 @@
-import {Preparation} from "../src/christmas/preparation";
-import {ToyType} from "../src/christmas/toyType";
+import fc from "fast-check";
+import {
+  GiftCategory,
+  GiftPreparation,
+  MAX_AGE_FOR_BABY,
+  MAX_AGE_FOR_CHILD,
+  MAX_AGE_FOR_TODDLER,
+  MAX_GIFTS_FOR_ELVES,
+  Preparation,
+} from "../src/christmas/preparation";
+import { ToyType } from "../src/christmas/toyType";
 
-describe('Preparation', () => {
-    test.each([
-        [-1, 'No gifts to prepare.'],
-        [0, 'No gifts to prepare.'],
-        [1, 'Elves will prepare the gifts.'],
-        [49, 'Elves will prepare the gifts.'],
-        [50, 'Santa will prepare the gifts.']
-    ])('prepareGifts should return the correct preparation message for %d gifts', (numberOfGifts, expected) => {
-        expect(Preparation.prepareGifts(numberOfGifts)).toBe(expected);
+describe("Preparation", () => {
+  describe("when preparing", () => {
+    it("no one needs to do anything when there are no gifts", () => {
+      fc.assert(
+        fc.property(fc.integer({ max: 0 }), (numberOfGifts) => {
+          expect(Preparation.prepareGifts(numberOfGifts)).toBe(
+            GiftPreparation.NO_ONE
+          );
+        })
+      );
     });
 
-    test.each([
-        [1, 'Baby'],
-        [3, 'Toddler'],
-        [6, 'Child'],
-        [13, 'Teen']
-    ])('categorizeGift should return the correct category for age %d', (age, expectedCategory) => {
-        expect(Preparation.categorizeGift(age)).toBe(expectedCategory);
+    it(`elves need to prepare when number of gifts is below ${MAX_GIFTS_FOR_ELVES}`, () => {
+      fc.assert(
+        fc.property(
+          fc.integer({ min: 1, max: MAX_GIFTS_FOR_ELVES - 1 }),
+          (numberOfGifts) => {
+            expect(Preparation.prepareGifts(numberOfGifts)).toBe(
+              GiftPreparation.ELVES
+            );
+          }
+        )
+      );
     });
 
-    test.each([
-        [ToyType.EDUCATIONAL, 25, 100, true],
-        [ToyType.FUN, 30, 100, true],
-        [ToyType.CREATIVE, 20, 100, true],
-        [ToyType.EDUCATIONAL, 20, 100, false],
-        [ToyType.FUN, 29, 100, false],
-        [ToyType.CREATIVE, 15, 100, false]
-    ])('ensureToyBalance should return %s for toy type %s with %d toys out of %d total toys', (toyType, toysCount, totalToys, expected) => {
-        expect(Preparation.ensureToyBalance(toyType, toysCount, totalToys)).toBe(expected);
+    it(`santa need to prepare the gifts when more than ${MAX_GIFTS_FOR_ELVES}`, () => {
+      fc.assert(
+        fc.property(
+          fc.integer({ min: MAX_GIFTS_FOR_ELVES }),
+          (numberOfGifts) => {
+            expect(Preparation.prepareGifts(numberOfGifts)).toBe(
+              GiftPreparation.SANTA
+            );
+          }
+        )
+      );
     });
+  });
+
+  describe.each([
+    [0, MAX_AGE_FOR_BABY, GiftCategory.BABY],
+    [MAX_AGE_FOR_BABY + 1, MAX_AGE_FOR_TODDLER, GiftCategory.TODDLER],
+    [MAX_AGE_FOR_TODDLER + 1, MAX_AGE_FOR_CHILD, GiftCategory.CHILD],
+    [MAX_AGE_FOR_CHILD + 1, undefined, GiftCategory.TEEN],
+  ])("when categorizing", (min, max, expected) => {
+    describe(`and age is above ${min} but below or equal to ${
+      max ?? "infinity"
+    }`, () => {
+      it(`should categorize for ${expected}`, () => {
+        fc.assert(
+          fc.property(fc.integer({ min, max }), (age) => {
+            expect(Preparation.categorizeGift(age)).toBe(expected);
+          })
+        );
+      });
+    });
+  });
+
+  test.each([
+    [ToyType.EDUCATIONAL, 25, 100, true],
+    [ToyType.FUN, 30, 100, true],
+    [ToyType.CREATIVE, 20, 100, true],
+    [ToyType.EDUCATIONAL, 20, 100, false],
+    [ToyType.FUN, 29, 100, false],
+    [ToyType.CREATIVE, 15, 100, false],
+    [undefined, 15, 100, false],
+    [null, 15, 100, false],
+  ])(
+    "ensureToyBalance should return %s for toy type %s with %d toys out of %d total toys",
+    (toyType, toysCount, totalToys, expected) => {
+      expect(Preparation.ensureToyBalance(toyType, toysCount, totalToys)).toBe(
+        expected
+      );
+    }
+  );
 });
