fd03692dac39e572856b9f76c408d31c64b754c6
Lukasz Kokot <lkokot@kumojin.com>
2024-12-12 09:05:40 -0500

    Format files

--- exercise/ts/day11/src/christmas/preparation.ts
+++ exercise/ts/day11/src/christmas/preparation.ts
@@ -1,38 +1,42 @@
-import {ToyType} from "./toyType";
+import { ToyType } from "./toyType";
 
 export class Preparation {
-    static prepareGifts(numberOfGifts: number): string {
-        if (numberOfGifts <= 0) {
-            return 'No gifts to prepare.';
-        } else if (numberOfGifts < 50) {
-            return 'Elves will prepare the gifts.';
-        }
-        return 'Santa will prepare the gifts.';
+  static prepareGifts(numberOfGifts: number): string {
+    if (numberOfGifts <= 0) {
+      return "No gifts to prepare.";
+    } else if (numberOfGifts < 50) {
+      return "Elves will prepare the gifts.";
     }
+    return "Santa will prepare the gifts.";
+  }
 
-    static categorizeGift(age: number): string {
-        if (age <= 2) {
-            return 'Baby';
-        } else if (age <= 5) {
-            return 'Toddler';
-        } else if (age <= 12) {
-            return 'Child';
-        }
-        return 'Teen';
+  static categorizeGift(age: number): string {
+    if (age <= 2) {
+      return "Baby";
+    } else if (age <= 5) {
+      return "Toddler";
+    } else if (age <= 12) {
+      return "Child";
     }
+    return "Teen";
+  }
 
-    static ensureToyBalance(toyType: ToyType, toysCount: number, totalToys: number): boolean {
-        const typePercentage = toysCount / totalToys;
+  static ensureToyBalance(
+    toyType: ToyType,
+    toysCount: number,
+    totalToys: number
+  ): boolean {
+    const typePercentage = toysCount / totalToys;
 
-        switch (toyType) {
-            case ToyType.EDUCATIONAL:
-                return typePercentage >= 0.25;
-            case ToyType.FUN:
-                return typePercentage >= 0.30;
-            case ToyType.CREATIVE:
-                return typePercentage >= 0.20;
-            default:
-                return false;
-        }
+    switch (toyType) {
+      case ToyType.EDUCATIONAL:
+        return typePercentage >= 0.25;
+      case ToyType.FUN:
+        return typePercentage >= 0.3;
+      case ToyType.CREATIVE:
+        return typePercentage >= 0.2;
+      default:
+        return false;
     }
+  }
 }
